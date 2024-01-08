842c802eb8edfa15cdbb702dee4ddbf3e3597394
LeondavidZipp <leondavidzipp@gmx.de>
2024-01-08 15:02:36 +0100

    nothing special

--- src/make_map.c
+++ src/make_map.c
@@ -6,7 +6,7 @@
 /*   By: lzipp <lzipp@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/08 17:32:58 by lzipp             #+#    #+#             */
-/*   Updated: 2024/01/08 11:09:36 by lzipp            ###   ########.fr       */
+/*   Updated: 2024/01/08 15:02:31 by lzipp            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,12 +18,13 @@ static void	free_values(char **values)
 	int		i;
 
 	i = -1;
-	while (values[++i])
-	{
-		printf("string freed: %s\n", values[i]);
-		free(values[i]);
-	}
-	printf("freeing values\n");
+
+	// while (values[++i])
+	// {
+	// 	printf("string freed: |%s|\n", values[i]);
+	// 	// free(values[i]);
+	// }
+	// printf("freeing values\n");
 	free(values);
 }
 
@@ -55,25 +56,32 @@ static char	**remove_newlines(char **values)
 	int		i;
 	char	*temp;
 
-	i = 0;
-	while (values[i])
+	i = -1;
+	while (values[++i])
 	{
 		if (values[i][0] == '\n')
 		{
 			temp = ft_strtrim(values[i], "\n");
-			if (!temp)
-				return (NULL);
+			printf("string trimmed: |%s|\n", temp);
 			free(values[i]);
+			if (!temp)
+			{
+				free_values(values);
+				return (NULL);
+			}
 			values[i] = temp;
 			if (!values[i])
+			{
+				free_values(values);
 				return (NULL);
+			}
 			free(temp);
 		}
-		i++;
 	}
-	printf("all good\n");
 	return (values);
 }
+
+static char **
 
 static t_map	*generate_map(char **values)
 {
@@ -104,7 +112,6 @@ static t_map	*fill_map(t_map *map, char **values)
 
 	i = 0;
 	row = 0;
-	printf("value: %s\n", values[20]);
 	while (values[i])
 	{
 		col = 0;
@@ -116,19 +123,18 @@ static t_map	*fill_map(t_map *map, char **values)
 			write(2, "Error\n", 6);
 			exit(1);
 		}
-		while (values[i][0] != '\n')
+		// while (values[i][0] != '\n')
+		while (col < map->width)
 		{
 			printf("writing from index i: %d\n", i);
-			printf("---\n");
-			if (i == 20)
-				printf("at pos 20: %s\n", values[i]);
-			map->rows[row][col] = *make_point(row, col, ft_atoi(values[i]), WHITE);
+			// printf("---\n");
+			map->rows[i / map->height][col] = *make_point(row, col, ft_atoi(values[i]), WHITE);
+			// map->rows[row][col] = *make_point(row, col, ft_atoi(values[i]), WHITE);
+
 			col++;
 			i++;
 		}
 		row++;
-		// if (values[i] && values[i][0] == '\n')
-		i++;
 	}
 	return (map);
 }
@@ -142,16 +148,30 @@ t_map	*make_map(char *filename)
 
 	lines = read_lines_from_file(filename);
 	values_step_1 = ft_split(lines, ' ');
+	int i = 0;
+	while (values_step_1[i])
+	{
+		printf("string 1: |%s|\n", values_step_1[i]);
+		// if (i % 10 == 0)
+			// printf("----------\n");
+		i++;
+	}
 	free(lines);
-	printf("have gotten here 1\n");
+	// printf("have gotten here 1\n");
 	if (!values_step_1)
 	{
 		write(2, "Error\n", 6);
 		exit(1);
 	}
 	values_step_2 = remove_newlines(values_step_1);
-	printf("have gotten here 2\n");
-	free_values(values_step_1);
+	i = 0;
+	while (values_step_2[i])
+	{
+		printf("string 2: |%s|\n", values_step_2[i]);
+		i++;
+	}
+	// printf("have gotten here 2\n");
+	// free_values(values_step_1);
 	if (!values_step_2)
 	{
 		write(2, "Error\n", 6);
@@ -167,18 +187,18 @@ t_map	*make_map(char *filename)
 int main() {
     char *filename = "../test_maps/10-2.fdf";
     t_map *map = make_map(filename);
-	printf("Map created successfully.\n");
+	// printf("Map created successfully.\n");
 
     if (map) {
         // printf("Map created successfully.\n");
-        printf("Map height: %d\n", map->height);
-        printf("Map width: %d\n", map->width);
+        // printf("Map height: %d\n", map->height);
+        // printf("Map width: %d\n", map->width);
         // Print the map
         for (int i = 0; i < map->height; i++) {
             for (int j = 0; j < map->width; j++) {
                 // printf("x: %d \n", map->rows[i][j].x);
 				// printf("y: %d \n", map->rows[i][j].y);
-				printf("z: %d \n", map->rows[i][j].z);
+				// printf("z: %d \n", map->rows[i][j].z);
 				// printf("row %d\n", i);
             }
             printf("\n##############\n");
