31b0952109ea22aa4f9d91b8b9f0370806319c9b
LeondavidZipp <leondavidzipp@gmx.de>
2024-01-17 13:06:47 +0100

    finished matrix operations

--- src/matrix_operations.c
+++ src/matrix_operations.c
@@ -6,13 +6,23 @@
 /*   By: lzipp <lzipp@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/17 12:31:55 by lzipp             #+#    #+#             */
-/*   Updated: 2024/01/17 12:32:33 by lzipp            ###   ########.fr       */
+/*   Updated: 2024/01/17 13:06:15 by lzipp            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../fdf.h"
 
-double	**make_matrix(void)
+void	free_projection(double **matrix)
+{
+	int			i;
+
+	i = -1;
+	while (++i < 3)
+		free(matrix[i]);
+	free(matrix);
+}
+
+double	**make_projection1(void)
 {
 	double	**matrix;
 
@@ -31,30 +41,55 @@ double	**make_matrix(void)
 	return (matrix);
 }
 
-void	multiply_matrix(double **matrix1, double **matrix2)
+double	**make_projection2(void)
 {
-	double	**result;
-	int		i;
-	int		j;
-	int		k;
+	double	**matrix;
 
-	result = ft_calloc(3, sizeof(double *));
-	i = 0;
-	while (i < 3)
+	matrix = ft_calloc(3, sizeof(double *));
+	matrix[0] = ft_calloc(3, sizeof(double));
+	matrix[1] = ft_calloc(3, sizeof(double));
+	matrix[2] = ft_calloc(3, sizeof(double));
+	matrix[1][0] = 1;
+	matrix[1][1] = 1;
+	return (matrix);
+}
+
+t_3d_point	*multiply_projection1(t_3d_point *point)
+{
+	t_3d_point		*result;
+	double			**projection1;
+
+	result = ft_calloc(1, sizeof(t_3d_point));
+	if (!result)
+		return (NULL);
+	projection1 = make_projection1();
+	if (!projection1)
 	{
-		result[i] = ft_calloc(3, sizeof(double));
-		j = 0;
-		while (j < 3)
-		{
-			k = 0;
-			while (k < 3)
-			{
-				result[i][j] += matrix1[i][k] * matrix2[k][j];
-				k++;
-			}
-			j++;
-		}
-		i++;
+		free(result);
+		return (NULL);
 	}
-	matrix1 = result;
+	result->x = 1 / sqrt(6) * (projection1[0][0] * point->x + projection1[0][1]
+			* point->y + projection1[0][2] * point->z);
+	result->y = 1 / sqrt(6) * (projection1[1][0] * point->x + projection1[1][1]
+			* point->y + projection1[1][2] * point->z);
+	result->z = 1 / sqrt(6) * (projection1[2][0] * point->x + projection1[2][1]
+			* point->y + projection1[2][2] * point->z);
+	free_projection(projection1);
+	return (result);
+}
+
+t_2d_point	*multiply_projeftion2(t_3d_point *point)
+{
+	t_2d_point		*result;
+	double			**projection2;
+
+	projection2 = make_projection2();
+	if (!projection2)
+		return (NULL);
+	result->x = projection2[0][0] * point->x + projection2[0][1] * point->y
+		+ projection2[0][2] * point->z;
+	result->y = projection2[1][0] * point->x + projection2[1][1] * point->y
+		+ projection2[1][2] * point->z;
+	free_projection(projection2);
+	return (result);
 }
