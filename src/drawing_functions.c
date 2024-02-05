230826bdd4959f099aa899519c55f8f8e7645ca4
LeondavidZipp <leondavidzipp@gmx.de>
2024-02-05 16:57:38 +0100

    no segfaults, recognizes wrong maps

--- src/drawing_functions.c
+++ src/drawing_functions.c
@@ -6,11 +6,32 @@
 /*   By: lzipp <lzipp@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/17 14:11:12 by lzipp             #+#    #+#             */
-/*   Updated: 2024/02/05 16:38:16 by lzipp            ###   ########.fr       */
+/*   Updated: 2024/02/05 16:55:21 by lzipp            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../fdf.h"
+
+static void	draw_line(t_2d_point *start, t_2d_point *end, mlx_image_t *img);
+static void	draw_multiple_dots(t_app_data *a_d);
+static void	draw_one_dot(t_2d_point *point, mlx_image_t *img);
+
+void	draw_map(t_app_data *a_d)
+{
+	a_d->image = mlx_new_image(a_d->mlx, a_d->window_width, a_d->window_height);
+	if (!a_d->image)
+	{
+		free_app_data(a_d);
+		exit(1);
+	}
+	map_3d_to_2d(a_d);
+	if (ft_null_terminated_arr_len((void **)a_d->map) == 1
+		&& ft_null_terminated_arr_len((void **)a_d->map[0]) == 1)
+		draw_one_dot(a_d->map[0][0]->projection, a_d->image);
+	else
+		draw_multiple_dots(a_d);
+	mlx_image_to_window(a_d->mlx, a_d->image, 0, 0);
+}
 
 static t_line	*init_line(t_2d_point *start, t_2d_point *end)
 {
@@ -36,6 +57,48 @@ static t_line	*init_line(t_2d_point *start, t_2d_point *end)
 	line->x = start->x;
 	line->y = start->y;
 	return (line);
+}
+
+static void	draw_one_dot(t_2d_point *point, mlx_image_t *img)
+{
+	if (point->x <= img->height && point->y <= img->width)
+		mlx_put_pixel(img, point->y, point->x, point->color);
+	if (point->x + 1 <= img->height && point->y <= img->width)
+		mlx_put_pixel(img, point->y, point->x + 1, point->color);
+	if (point->x <= img->height && point->y + 1 <= img->width)
+		mlx_put_pixel(img, point->y + 1, point->x, point->color);
+	if (point->x - 1 <= img->height && point->y <= img->width)
+		mlx_put_pixel(img, point->y, point->x - 1, point->color);
+	if (point->x <= img->height && point->y - 1 <= img->width)
+		mlx_put_pixel(img, point->y - 1, point->x, point->color);
+}
+
+static void	draw_multiple_dots(t_app_data *a_d)
+{
+	int	x;
+	int	y;
+
+	a_d->image = mlx_new_image(a_d->mlx, a_d->window_width, a_d->window_height);
+	if (!a_d->image)
+	{
+		free_app_data(a_d);
+		exit(1);
+	}
+	map_3d_to_2d(a_d);
+	x = -1;
+	while (a_d->map[++x])
+	{
+		y = -1;
+		while (a_d->map[x][++y])
+		{
+			if (a_d->map[x][y + 1])
+				draw_line(a_d->map[x][y]->projection, a_d->map[x][y + 1]
+					->projection, a_d->image);
+			if (a_d->map[x + 1])
+				draw_line(a_d->map[x][y]->projection, a_d->map[x + 1][y]
+					->projection, a_d->image);
+		}
+	}
 }
 
 static void	draw_line(t_2d_point *start, t_2d_point *end, mlx_image_t *img)
@@ -65,31 +128,31 @@ static void	draw_line(t_2d_point *start, t_2d_point *end, mlx_image_t *img)
 	free(line);
 }
 
-void	draw_map(t_app_data *a_d)
-{
-	int	x;
-	int	y;
+// void	draw_map(t_app_data *a_d)
+// {
+// 	int	x;
+// 	int	y;
 
-	a_d->image = mlx_new_image(a_d->mlx, a_d->window_width, a_d->window_height);
-	if (!a_d->image)
-	{
-		free_app_data(a_d);
-		exit(1);
-	}
-	map_3d_to_2d(a_d);
-	x = -1;
-	while (a_d->map[++x])
-	{
-		y = -1;
-		while (a_d->map[x][++y])
-		{
-			if (a_d->map[x][y + 1])
-				draw_line(a_d->map[x][y]->projection, a_d->map[x][y + 1]
-					->projection, a_d->image);
-			if (a_d->map[x + 1])
-				draw_line(a_d->map[x][y]->projection, a_d->map[x + 1][y]
-					->projection, a_d->image);
-		}
-	}
-	mlx_image_to_window(a_d->mlx, a_d->image, 0, 0);
-}
+// 	a_d->image = mlx_new_image(a_d->mlx, a_d->window_width, a_d->window_height);
+// 	if (!a_d->image)
+// 	{
+// 		free_app_data(a_d);
+// 		exit(1);
+// 	}
+// 	map_3d_to_2d(a_d);
+// 	x = -1;
+// 	while (a_d->map[++x])
+// 	{
+// 		y = -1;
+// 		while (a_d->map[x][++y])
+// 		{
+// 			if (a_d->map[x][y + 1])
+// 				draw_line(a_d->map[x][y]->projection, a_d->map[x][y + 1]
+// 					->projection, a_d->image);
+// 			if (a_d->map[x + 1])
+// 				draw_line(a_d->map[x][y]->projection, a_d->map[x + 1][y]
+// 					->projection, a_d->image);
+// 		}
+// 	}
+// 	mlx_image_to_window(a_d->mlx, a_d->image, 0, 0);
+// }
