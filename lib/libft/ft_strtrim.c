3fa092fa89510ffa214b32aaf35aedd3bc726e87
LeondavidZipp <leondavidzipp@gmx.de>
2024-02-05 16:26:54 +0100

    nothing special

--- lib/libft/ft_strtrim.c
+++ lib/libft/ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: lzipp <lzipp@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/08 11:33:36 by lzipp             #+#    #+#             */
-/*   Updated: 2024/02/05 16:24:44 by lzipp            ###   ########.fr       */
+/*   Updated: 2024/02/05 16:26:02 by lzipp            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,29 +29,58 @@ static int	in_set(char c, char const *set)
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
-	int		i;
-	int		front_i;
-	int		rev_i;
+	size_t	i;
+	size_t	rev_i;
+	size_t	j;
 	char	*res;
+	size_t	total;
 
-	if (!s1 || !set)
-		return (NULL);
-	if (ft_strlen(s1) == 0)
-		return ((char *)s1);
-	front_i = 0;
-	rev_i = ft_strlen(s1) - 1;
-	while (s1[front_i] && in_set(s1[front_i], set))
-		front_i++;
-	while (rev_i > 0 && s1[rev_i] && in_set(s1[rev_i], set))
-		rev_i--;
-	res = (char *)ft_calloc(rev_i + 1 - front_i, sizeof(char));
-	if (!res)
+	if (!s1)
 		return (NULL);
 	i = 0;
-	while (front_i <= rev_i)
-		res[i++] = s1[front_i++];
+	rev_i = ft_strlen(s1) - (ft_strlen > 0);
+	while (s1[i] && in_set(s1[i], set))
+		i++;
+	while (rev_i > 0 && s1[rev_i] && in_set(s1[rev_i], set))
+		rev_i--;
+	total = 0;
+	if (rev_i > i)
+		total = rev_i - i + 1;
+	res = (char *)malloc((total + 1) * sizeof(char));
+	if (!res)
+		return (NULL);
+	j = 0;
+	while (i <= rev_i && rev_i > 0)
+		res[j++] = s1[i++];
+	res[j] = '\0';
 	return (res);
 }
+
+// char	*ft_strtrim(char const *s1, char const *set)
+// {
+// 	int		i;
+// 	int		front_i;
+// 	int		rev_i;
+// 	char	*res;
+
+// 	if (!s1 || !set)
+// 		return (NULL);
+// 	if (ft_strlen(s1) == 0)
+// 		return ((char *)s1);
+// 	front_i = 0;
+// 	rev_i = ft_strlen(s1) - 1;
+// 	while (s1[front_i] && in_set(s1[front_i], set))
+// 		front_i++;
+// 	while (rev_i > 0 && s1[rev_i] && in_set(s1[rev_i], set))
+// 		rev_i--;
+// 	res = (char *)ft_calloc(rev_i + 1 - front_i, sizeof(char));
+// 	if (!res)
+// 		return (NULL);
+// 	i = 0;
+// 	while (front_i <= rev_i)
+// 		res[i++] = s1[front_i++];
+// 	return (res);
+// }
 
 // #include <stdio.h>
 // #include <stdlib.h>
